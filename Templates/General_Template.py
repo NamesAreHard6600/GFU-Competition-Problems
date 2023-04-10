54b21a9a84beba6ecd1915ed78f0010418766486
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2023-04-10 09:38:30 -0700

    General Changes

--- Templates/General_Template.py
+++ Templates/General_Template.py
@@ -1,23 +1,26 @@
-true = True
-false = False
-
-def fileReading():
+#Line 16
+def input_reading():
   datatable = []
-  f = open("letter.in", 'r')
+  num = int(input().strip())
+  datatable = [input().strip() for _ in range(num)]
+  return datatable
+  
+def file_reading():
+  datatable = []
+  f = open(FILENAME, 'r')
   num = int(f.readline().strip())
   datatable = [f.readline().strip() for _ in range(num)]
   f.close()
   return datatable
 
-def inputReading():
-  datatable = []
-  num = int(input().strip())
-  datatable = [input().strip() for _ in range(num)]
-  return datatable
+FILENAME = "letter.in"
+READ = file_reading
+#READ = input_reading
 
-datatable = fileReading()
-# datatable = inputReading()
+true = True
+false = False
 
-for iteration in datatable:
-  
-  print(iteration)
+datatable = READ()
+
+for itr in datatable:
+  print(itr)
