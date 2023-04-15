53ce55cb2ac6aa431698af51268bd473ab85dba7
ProfMonkey07 <67666529+ProfMonkey07@users.noreply.github.com>
2023-04-15 12:29:04 -0700

    Add files via upload

--- 2023/2.py
+++ 2023/2.py
@@ -1,32 +1,29 @@
 import math
 
-def input_reading():
-    datatable = []
+def read_input():
+    table = []
     num = int(input().strip())
-    datatable = [input().strip() for _ in range(num*2)]
-    datatable.insert(0,num)
-    return datatable
+    for i in range(num * 2):
+        table.append(input().strip())
+    newtable = []
+    for i in range(len(table)):
+        if i % 2 != 0:
+            newtable.append(table[i])
+    return newtable
 
-def part(x,y):
-    if x == 0 or y == 0:
-        return 0
-    return int(x*y/math.gcd(x,y))
-
-def lcm(arr):
-    if len(arr) == 0:
-        return 1
-    if len(arr) == 1:
-        return arr[0]
-    ret = part(arr[0],arr[1])
-    for x in arr[2:]:
-        ret = part(ret,x)
-    return ret
-
-
-data = iter(input_reading())
-
-for x in range(int(next(data))):
-    next(data)
-    arr = [int(x) for x in next(data).split()]
-    print(lcm(arr))
-
+def find_lcm(nums):
+    numbers = []
+    for item in nums.split(" "):
+        numbers.append(int(item))
+    nums = numbers
+    x = 1
+    lcm = nums[0]
+    while x < len(nums):
+        lcm = int((lcm * nums[x]) / math.gcd(lcm, nums[x]))
+        x += 1
+    print(lcm)
+#all gnomes have a pace of Gi
+#at each int multiple of Gi the gnome(G) will do a dance move
+#given a set of gnomes, what is the first point in time when all perform simultaneous dance move
+for set in read_input():
+    find_lcm(set)
