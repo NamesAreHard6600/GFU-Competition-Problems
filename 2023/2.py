f088b08ece2e1f2fd6ebdd9c2e1151b8d2eba18c
ProfMonkey07 <67666529+ProfMonkey07@users.noreply.github.com>
2023-04-15 12:53:51 -0700

    Add files via upload

--- 2023/2.py
+++ 2023/2.py
@@ -16,12 +16,16 @@ def find_lcm(nums):
     for item in nums.split(" "):
         numbers.append(int(item))
     nums = numbers
-    x = 1
-    lcm = nums[0]
-    while x < len(nums):
-        lcm = int((lcm * nums[x]) / math.gcd(lcm, nums[x]))
-        x += 1
-    print(lcm)
+    if len(nums) == 1:
+        print(nums[0])
+    else:
+        x = 1
+        lcm = nums[0]
+        while x < len(nums):
+            lcm = int(lcm * nums[x]) // math.gcd(lcm, nums[x])
+            x += 1
+            print(str((x / len(nums)) * 100))
+        print(lcm)
 #all gnomes have a pace of Gi
 #at each int multiple of Gi the gnome(G) will do a dance move
 #given a set of gnomes, what is the first point in time when all perform simultaneous dance move
