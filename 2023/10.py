69193b65a91b4be6e0d6c4791cc579311162806a
ProfMonkey07 <67666529+ProfMonkey07@users.noreply.github.com>
2023-04-15 12:13:48 -0700

    Add files via upload

--- 2023/10.py
+++ 2023/10.py
@@ -4,26 +4,20 @@
 #cannot move over a gap longer than 2 meters
 #find minimum number of meter long strips of road that he must fill it to safely move the piano
 def numpaved(road):
+    r = []
+    for tile in road:
+        r.append(tile)
+    x = 2
     total = 0
-    gaps = []
-    ongap = False
-    currentgap = []
-    for i in range(len(road)):
-        if road[i] == ".":
-            if not ongap:
-                ongap = True
-            currentgap.append(".")
-        elif road[i] == "-":
-            if ongap:
-                ongap = False
-                currentgap = []
-                gaps.append(currentgap)
-    if ongap:
-        gaps.append(currentgap)
-    for gap in gaps:
-        if len(gap) > 2:
-            total += int(len(gap) / 3)
-    print(total)
+    if len(road) > 2:
+        while x < len(r):
+            if r[x] == "." and r[x - 1] == "." and r[x - 2] == ".":
+                total += 1
+                r[x] = "-"
+            x += 1
+        print(total)
+    else:
+        print(0)
 
 def readinput():
     num = int(input().strip())
