1ab0e378cb5235ce7d47c197da0f02b2a1962e8a
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2023-04-17 09:27:29 -0700

    Update 5-concerto.py

--- 2023/5-concerto.py
+++ 2023/5-concerto.py
@@ -1,10 +1,12 @@
+#Written by Jacob
+
 count = int(input())
 data = []
 numberCorrect = 0
 for i in range(count):
     data.append(input())
 for j in range(count):
-    if data[j].lower().count("minor") >= 2:
+    if data[j].count("minor") > 2:
         print("It\'s over Vee Valdee! I have the high ground!")
     else:
         print("You underestimate my music!")
